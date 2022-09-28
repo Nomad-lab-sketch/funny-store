ad05d9f2504e8e9b1f60a176d9bbef6a546f1dce
Sergei Rodin <sergei-rodin90@yandex.ru>
2022-09-28 12:04:39 +0300

    \/_/\

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SimpleButtonComponent } from './modules/simple-button/simple-button.component';
+import { SideNavigateComponent } from './components/side-navigate/side-navigate.component';
+import { SvgIconsComponent } from './modules/svg-icons/svg-icons.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SliderComponent } from './modules/slider/slider.component';
+import { AppRoutingModule } from './routes/app-routing.module';
+import { CatalogComponent } from './components/content/catalog/catalog.component';
+import { CardComponent } from './modules/card/card.component';
+import { CatalogListComponent } from './modules/catalog-list/catalog-list.component';
+import { ShoppingCartComponent } from './components/content/shopping-cart/shopping-cart.component';
+import { ImageComponent } from './modules/image/image.component';
+import { ToastComponent } from './modules/toast/toast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleButtonComponent,
+    SideNavigateComponent,
+    SvgIconsComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    CatalogComponent,
+    CardComponent,
+    CatalogListComponent,
+    ShoppingCartComponent,
+    ImageComponent,
+    ToastComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AngularSvgIconModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
